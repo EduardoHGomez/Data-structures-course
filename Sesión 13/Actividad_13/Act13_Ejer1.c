fb1bac6a9e966bca8c6e842324e6eda016ebf7f2
Eduardo <hurtado.josegomez@gmail.com>
2023-03-11 17:14:38 -0600

    First attemp to exercise (doesn't work)

--- Sesión 13/Actividad_13/Act13_Ejer1.c
+++ Sesión 13/Actividad_13/Act13_Ejer1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct STRNUM{
-    int n;
+    int number;
     struct STRNUM *next;
 };
 
@@ -12,15 +12,48 @@ typedef struct {
 
 int main()
 {
-    int n;
+    int limite, is_prime, SIZE = 1;
     printf("Ingrese un numero limite: ");
-    scanf("%d", &n);
+    scanf("%d", &limite);
 
-    for (int i = 0; i < n; i++)
+    LIST lista = {NULL, NULL};
+    struct STRNUM *ptr = NULL, *todestro = NULL;
+
+    //Establecer 2 como el primer número primo
+    lista.first = malloc(sizeof(struct STRNUM));
+    lista.first->number = 2;
+    lista.last->next = NULL;
+
+    //Hacer que ptr apunte al inicio de la lista
+    ptr = lista.first;
+
+    //Ciclo que revisa todo el ciclo
+    for (int i = 2; i < limite; i++)
     {
-        /* code */
+        is_prime = 1;
+        while (ptr != NULL)
+        
     }
     
+}
 
+void insert(LIST *l,int n)
+{
+	// Crear el nodo
+	struct STRNUM *new = malloc(sizeof(struct STRNUM));
 
+	// Poner los valores en la estructura
+	new->num = n;
+	new->next = NULL;
+
+	// Si first apunta a NULL es que la lista está vacía
+	// y hay que poner el nodo como el primero de la lista
+	if(l->first==NULL)
+		l->first = new;
+	else
+		l->last->next = new; // Si no, hay que ponerlo después
+							 // de last
+
+	l->last = new;	// last debe apuntar al nodo que acabamos
+					// de poner, que es el último
 }
