53780f640af0fa4dc549945863c19bcf5c40419f
Eduardo <hurtado.josegomez@gmail.com>
2023-03-11 15:22:08 -0600

    uppload

--- Sesión 13/Actividad_13/prueba.c
+++ Sesión 13/Actividad_13/prueba.c
@@ -7,34 +7,35 @@ int main()
     printf("Ingrese un numero: ");
     scanf("%d", &limite);
 
-    int SIZE = 1;
+    int SIZE = 4;
     int *lista_primos = malloc(SIZE * sizeof(int));
-    *(lista_primos) = 2;
+    *(lista_primos) = 2; *(lista_primos + 1) = 3; *(lista_primos + 2) = 5; *(lista_primos + 3) = 7;
 
-    int resultado;
+    int resultado, is_prime = 0;
     
     for (int i = 2; i <= limite; i++)
-    {
+    {   is_prime = 1;
         for (int j = 0; j < SIZE; j++)
         {   
-            
-            if (i % *(lista_primos + j) != 0 && i > 2)
+            if (i != *(lista_primos+j))
             {
-                
-                SIZE++;
-                realloc(lista_primos, SIZE*sizeof(int));
-                *(lista_primos + SIZE - 1) = i;
-                
+                printf("i = %d j = %d resultado = %d\n", i, *(lista_primos+j), i%*(lista_primos+j));
+                if (i%*(lista_primos+j) == 0)
+                    printf("no es primo\n");
             }
+            
         }
+        printf("\n");
         
     }
 
-    printf("--\nSIZE: %d\n", SIZE);
+/**
+ *     printf("--\nSIZE: %d\n", SIZE);
     for (int i = 0; i < SIZE; i++)
     {
         printf("%d ", *(lista_primos+i));
     }
+*/
     
     
     free(lista_primos);
