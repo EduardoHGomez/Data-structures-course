0bc1eb8e937c0908c5ee1ad7c568637e38ee11b9
Eduardo <hurtado.josegomez@gmail.com>
2023-03-11 16:40:35 -0600

    Program now works

--- Sesión 13/Actividad_13/prueba.c
+++ Sesión 13/Actividad_13/prueba.c
@@ -7,9 +7,10 @@ int main()
     printf("Ingrese un numero: ");
     scanf("%d", &limite);
 
-    int SIZE = 4;
+    int SIZE = 1;
     int *lista_primos = malloc(SIZE * sizeof(int));
-    *(lista_primos) = 2; *(lista_primos + 1) = 3; *(lista_primos + 2) = 5; *(lista_primos + 3) = 7;
+    *(lista_primos) = 2; 
+    //*(lista_primos + 1) = 3; *(lista_primos + 2) = 5; *(lista_primos + 3) = 7;
 
     int resultado, is_prime = 0;
     
@@ -17,15 +18,28 @@ int main()
     {   is_prime = 1;
         for (int j = 0; j < SIZE; j++)
         {   
-            if ((i != *(lista_primos+j)) && (i%*(lista_primos+j) == 0))  
+            if ((i != *(lista_primos+j)) && (i%*(lista_primos+j) == 0) && (i > 2))  
             {
                 is_prime = 0;
                 break;
             } 
         }
-        if (is_prime)
+        if (is_prime && i > 2){
             printf("%d es primo\n", i);
-        
+            SIZE++;
+            lista_primos = realloc(lista_primos, SIZE * sizeof(int));
+            if (lista_primos == NULL) 
+            {
+                printf("Error: no se pudo asignar memoria\n");
+                exit(1);
+            }
+            *(lista_primos + SIZE - 1) = i;
+        }
     }
-
-
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", lista_primos[i]);
+    }
+    
+    free(lista_primos);
+}
