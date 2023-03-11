5810c5b35866e3eaf3afe5f82934f672b1efed93
Eduardo <hurtado.josegomez@gmail.com>
2023-03-11 17:32:34 -0600

    works

--- Sesión 13/Actividad_13/Act13_Ejer1.c
+++ Sesión 13/Actividad_13/Act13_Ejer1.c
@@ -24,36 +24,45 @@ int main()
     lista.first->number = 2;
     lista.last->next = NULL;
 
-    //Hacer que ptr apunte al inicio de la lista
-    ptr = lista.first;
-
     //Ciclo que revisa todo el ciclo
     for (int i = 2; i < limite; i++)
     {
+        //Hacer que ptr apunte al inicio de la lista
+        ptr = lista.first;
+
         is_prime = 1;
-        while (ptr != NULL)
+        //Recorrer toda la lista de primos
+        for (int j = 0; j < SIZE; j++)
+        {
+            if ((i != ptr->number) && (i % ptr->number == 0))
+            {
+                ptr = ptr->next;
+                is_prime = 0;
+            }
+        }
+        
         
     }
     
 }
 
-void insert(LIST *l,int n)
+void insert(LIST *lista, int prime)
 {
 	// Crear el nodo
 	struct STRNUM *new = malloc(sizeof(struct STRNUM));
 
 	// Poner los valores en la estructura
-	new->num = n;
+	new->number = prime;
 	new->next = NULL;
 
 	// Si first apunta a NULL es que la lista está vacía
 	// y hay que poner el nodo como el primero de la lista
-	if(l->first==NULL)
-		l->first = new;
+	if(lista->first==NULL)
+		lista->first = new;
 	else
-		l->last->next = new; // Si no, hay que ponerlo después
+		lista->last->next = new; // Si no, hay que ponerlo después
 							 // de last
 
-	l->last = new;	// last debe apuntar al nodo que acabamos
+	lista->last = new;	// last debe apuntar al nodo que acabamos
 					// de poner, que es el último
 }
