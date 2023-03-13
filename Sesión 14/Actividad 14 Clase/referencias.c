3e14a5c82360d69cc76896bba0d711f3584a8dd1
Eduardo <hurtado.josegomez@gmail.com>
2023-03-13 14:27:59 -0600

    Referencing understood

--- Sesión 14/Actividad 14 Clase/referencias.c
+++ Sesión 14/Actividad 14 Clase/referencias.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+void change(int **n)
+{
+    *n = NULL;
+}
+
 
 
 int main()
 {
-    
+    int n = 5;
+    int *ptr = &n;    
 
+    printf("Direccion inicial: %p Direccion n: %p\n", ptr, &n);
+    change(&ptr);
+
+    printf("Direccion inicial: %p Direccion n: %p\n", ptr, &n);
+    
 }
