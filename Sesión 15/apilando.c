0fe6760c334cde234e04852d8cbf2e0835343787
Eduardo <hurtado.josegomez@gmail.com>
2023-03-13 10:07:24 -0600

    New pile

--- Sesión 15/apilando.c
+++ Sesión 15/apilando.c
@@ -8,9 +8,43 @@ struct STRNUM {
 
 int main()
 {
-    struct STRNUM *top = NULL, *new = NULL;
+    struct STRNUM *top = NULL, *new = NULL, *ptr = NULL, *destr = NULL;
     int num;
+    setbuf(stdout,NULL);
+
+    do
+    {
+        printf("Dame un numero (0 para terminar): ");
+        scanf("%d", &num);
 
 
+        if (num != 0)
+        {
+            new = malloc(sizeof(struct STRNUM));
+            new->data = num;
+            new->next = top;
+
+            top = new;
+        }
+        
+
+    } while (num != 0);
+    
+    ptr = top;
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+    
+    // Destruir los nodos
+    ptr = top;
+    while (ptr != NULL)
+    {
+        destr = ptr;
+        ptr = ptr->next;
+        free(destr);
+    }
+    
 
 }
