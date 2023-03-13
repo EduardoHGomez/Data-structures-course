be5ee30cab3debadef47dfc3846456bd85992ab3
Eduardo <hurtado.josegomez@gmail.com>
2023-03-13 10:00:44 -0600

    New files

---
+++ Sesión 15/apilando.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct STRNUM {
+    int data;
+    struct STRNUM *next;
+};
+
+int main()
+{
+    struct STRNUM *top = NULL, *new = NULL;
+    int num;
+
+
+
+}
