5b5ea14449194c56da5efd636fe911f7608cba66
Eduardo <hurtado.josegomez@gmail.com>
2023-03-17 14:44:17 -0600

    Restar

--- Sesión 14/Actividad 14 Clase/Ejer1_clase.c
+++ Sesión 14/Actividad 14 Clase/Ejer1_clase.c
@@ -12,41 +12,14 @@ typedef struct STRNODE {
     struct STRNODE *next;
 } node;
 
-typedef struct {
-    node *first;
-    node *last;
-} LIST;
-
 #define LISTINIT {NULL,NULL}
 //-----------Funciones----------
-void insert(LIST *list, int i);
 
 int main()
 {
     srand(time(NULL));
-    LIST list = LISTINIT;
+    node *list = NULL;
 
-    for(int i = 0; i < LIMIT; i++)
-    {
-        //int n = rand() % LIMIT;
-        insert(&list, i);
-    }
-
-    node *ptr = list.first;
-    for (int i = 0; i < LIMIT; i++)
-    {
-        printf("%d ", ptr->num);
-    }
     
 
-}
-
-void insert(LIST *list, int i)
-{
-    node *n = malloc(sizeof(node));
-
-    n->num = i;
-    n->next = NULL;
-    n->next = list->first;
-    list->first = n;
 }
