c95544a0abd46093e74e9432d98bfb6b67012a80
Eduardo <hurtado.josegomez@gmail.com>
2023-03-18 09:07:02 -0600

    New folders

--- Sesión 14/Actividad 14 Clase/Ejer1_clase.c
+++ Sesión 14/Actividad 14 Clase/Ejer1_clase.c
@@ -17,9 +17,14 @@ typedef struct {
     struct STRNODE *last;
 } LIST;
 
-#define LISTINIT {NULL, NULL}
+
 //-----------Funciones----------
 void insert(LIST *list, int i);
+void display(LIST list);
+int found(LIST list,int num);
+void destroy(LIST list);
+
+#define LISTINIT {NULL, NULL}
 
 int main()
 {
@@ -30,17 +35,11 @@ int main()
     for (int i = 0; i < LIMIT; i++)
     {
         n = rand() % LIMIT;
-        insert(&list, n);
+        if(!found(list,n))
+            insert(&list,n);
     }
-    
-    node *ptr = list.first;
-    while (ptr != NULL)
-    {
-        printf("%d ", ptr->num);
-        ptr = ptr->next;
-    }
-    
 
+    display(list);
 
 }
 
@@ -59,5 +58,45 @@ void insert(LIST *list, int i)
         list->last->next = n;
     }
     list->last = n;
+    
+}
+
+int found(LIST list,int num)
+{
+    node *ptr = list.first;
+    while (ptr != NULL)
+    {
+        if (ptr->num == num)
+        {
+            return 1;
+        }
+        
+        ptr = ptr->next;
+    }
+
+    return 0;   
+}
+
+void display(LIST list)
+{
+    node *ptr = list.first;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->num);
+        ptr = ptr->next;
+    }
+}
+
+void destroy(LIST list)
+{
+    node *next = NULL;
+
+    for (node *ptr = list.first; ptr != NULL; ptr = ptr->next) // Otra forma
+    {
+        next = ptr->next;
+        free(ptr);
+        ptr = next;
+
+    }
     
 }
