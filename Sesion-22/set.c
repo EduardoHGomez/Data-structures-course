b04a75341b1f1621591e307119124c69be908b4f
Eduardo <hurtado.josegomez@gmail.com>
2023-04-19 10:36:06 -0600

    set.c functions done

--- Sesion-22/set.c
+++ Sesion-22/set.c
@@ -1,48 +1,63 @@
 #include "set.h"
 
-SET set_create(COMPAREFUNC cf, PRINTFUNC pf)
+SET set_create(COMPAREFUNC cf,PRINTFUNC pf)
 {
-    SET new_set = malloc(sizeof(struct STRSET));
-    new_set->tree_root = NULL; // Inicia ocn el árbol vacío
+	SET new_set = malloc(sizeof(struct STRSET));
+	new_set->tree_root = NULL;	//Inicia con el árbol vacío
+	new_set->size = 0;
+	new_set->cf = cf;
+	new_set->pf = pf;
 
-    new_set->size = 0;
-    new_set->cf = cf;
-    new_set->pf = pf;
-
-    return new_set;
+	return new_set;
 }
 
-SET set_add(SET set, TYPE e)
+void set_add(SET set,TYPE e)
 {
-    if (tree_insert(set->tree_root, e))
-    {
-        set->size = set->size + 1;
-    }
-}
-
-// Funciones para el manejo de los árboles
-void treeinsert(struct STRTNODE **root, int n)
-{
-    if(*root == NULL)
-    {
-        *root = malloc(sizeof(struct STRTNODE));
-        (*root)->elem = n;
-        (*root)->right = NULL;
-        (*root)->left = NULL;
-    
-    }
-    else if (cf,(e, (*root)->elem) < 0)
-    {
-        treeinsert(&(*root)->left, n);
-    }
-    else if (n > (*root) -> elem)
-    {
-        treeinsert(&(*root)->right, n);
-    }
-
+	if(tree_insert(&set->tree_root,e,set->cf))
+		set->size = set->size + 1;
 }
 
 int set_size(SET set)
 {
-    return set->size;
+	return set->size;
+}
+
+void set_print(SET set)
+{
+	tree_print(0,set->tree_root,set->pf);
+}
+
+// Funciones para el manejo de los árboles
+int tree_insert(struct STRTNODE **root,TYPE e,COMPAREFUNC cf)
+{
+	int inserted = 0;
+	if(*root==NULL)
+	{
+		*root = malloc(sizeof(struct STRTNODE));
+		(*root)->elem = e;
+		(*root)->left = NULL;
+		(*root)->right = NULL;
+		inserted = 1;
+	}
+	else if(cf(e,(*root)->elem)<0)
+		inserted = tree_insert(&(*root)->left,e,cf);
+	else if(cf(e,(*root)->elem)>0)
+		inserted = tree_insert(&(*root)->right,e,cf);
+
+	return inserted;
+}
+
+void tree_print(int level,struct STRTNODE *root,PRINTFUNC pf)
+{
+	int i;
+	if(root!=NULL)
+	{
+		tree_print(level+1,root->right,pf);
+
+		for(i=0;i<level;i++)
+			printf("\t");
+		pf(root->elem);
+
+		tree_print(level+1,root->left,pf);
+	}
 }
