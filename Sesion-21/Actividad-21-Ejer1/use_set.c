13c7c169a6caa2f813318324a29f253727d59da9
Eduardo <hurtado.josegomez@gmail.com>
2023-04-23 00:55:26 -0600

    New set structure

---
+++ Sesion-21/Actividad-21-Ejer1/use_set.c
@@ -0,0 +1,81 @@
+/*
+ * use_set.c
+ *
+ *  Created on: 19 abr 2023
+ *      Author: jluis
+ */
+#include <stdio.h>
+#include <string.h>
+#include "set.h"
+
+int comparador_enteros(TYPE a,TYPE b);
+int color_code(char *color_name);
+int comparador_colores(TYPE color1,TYPE color2);
+void print_entero(TYPE data);
+void print_color(TYPE data);
+
+int main()
+{
+	SET s1 = set_create(comparador_colores,
+			print_color);
+
+
+	set_add(s1,string_create("ROJO"));
+	set_add(s1,string_create("NEGRO"));
+	set_add(s1,string_create("VERDE"));
+	set_add(s1,string_create("ROJO"));
+	set_add(s1,string_create("NEGRO"));
+	set_add(s1,string_create("VERDE"));
+	set_add(s1,string_create("AZUL"));
+
+
+	printf("Tamaño del conjunto %d\n",set_size(s1));
+
+	set_print(s1);
+
+}
+
+/*
+ * Esta función regresa
+ * 	Un valor positivo si a es mayor que b
+ * 	Un valor negativo si a es menor que b
+ * 	0 si a y b son iguales
+ */
+int comparador_enteros(TYPE a,TYPE b)
+{
+	int ia = *((int *) a);
+	int ib = *((int *) b);
+	return ia - ib;
+}
+
+int color_code(char *color_name)
+{
+	int retval = 0;
+	if(strcmp(color_name,"NEGRO")==0)
+		retval = 10;
+	if(strcmp(color_name,"ROJO")==0)
+		retval = 20;
+	if(strcmp(color_name,"VERDE")==0)
+		retval = 30;
+	if(strcmp(color_name,"AZUL")==0)
+		retval = 40;
+
+	return retval;
+}
+
+int comparador_colores(TYPE color1,TYPE color2)
+{
+	return(color_code(color1) - color_code(color2));
+}
+
+void print_entero(TYPE data)
+{
+	printf("%d\n", *((int *)data));
+}
+
+void print_color(TYPE data)
+{
+	printf("{");
+	printf("\"color\":\"%s\"",(char *) data);
+	printf("}\n");
+}
