614b2fbe4788f5fc657df810743c3c9a69aabb21
Eduardo <hurtado.josegomez@gmail.com>
2023-05-07 17:20:13 -0600

    hash code

--- Sesion-24/hash.c
+++ Sesion-24/hash.c
@@ -2,65 +2,74 @@
 #include <ctype.h>
 #include <string.h>
 
-#define 12
-
 struct STRMLNODE {
-    char key[60];
-    char data[80];
-
-    struct STRLNODE *next;
-    struct STRLNODE *prev;
+	char key[60];
+	char data[80];
+	struct STRMLNODE *next;
+	struct STRMLNODE *prev;
 };
 
-struct STRLIST {
-    struct STRMLNODE *first;
-    struct STRMLNODE *last;
+struct STRMAPLIST {
+	struct STRMLNODE *first;
+	struct STRMLNODE *last;
 };
+
+#define M 12
+
+struct STRMAPLIST table[M];
 
 unsigned long long hash(char *s);
 
+
 int main()
 {
-    struct STRLIST l1;
+	char key[60];
+	char nombre[80];
+	unsigned long long hashcode;
+	int p;
 
-    char key[60];
-    unsigned long long hashcode;
-    char c;
-    int p;
-    
-    while (1)
-    {
-        printf("Dame una llave, FIN para terminar : ");
-        scanf("%s", key);
-        if(strcmp(key, "fin") == 0)
-            break;
-        p = hash(key); 
-        printf("El codigo es %016llx\n", hashcode);
-        
-    }
-    
+	setbuf(stdout,NULL);
+
+	while(1)
+	{
+		printf("Dame una llave, FIN para terminar :");
+		scanf("%s",key);
+		if(strcmp(key,"FIN")==0)
+			break;
+		printf("Nombre completo :");
+		scanf("%s",nombre);
+
+		hashcode = hash(key);
+		printf("El código es %016llX\n",hashcode);
+		p = (int) (hashcode % M);
+		printf("Posición en la tabla(p) es %d\n",p);
+
+		/* Insertar un nodo en la lista p correspondiente
+		 * que contenga la llave y el nombre
+		 */
+	}
 }
 
 unsigned long long hash(char *s)
 {
-    int len = strlen(s);
-    unsigned long long retcode = 0;
-    char c;
+	int len = strlen(s);
+	int i=0;
+	unsigned long long retcode = 0;
+	char c;
 
-    if (len > 12)
-        len = 12;
+	if(len>12)
+		len = 12;
 
-    int i = 0;
-    while (i<len && s[i] != '\0')
-    {
-        c = toupper(s[i]);
-        if (c >= 'A' && c<= 'Z')
-        {
-            retcode = retcode << 5;
-            retcode = retcode | (c & 0x1F);
-        }
-        i++;   
-    }
-    
-    
+	while(i<len && s[i]!='\0')
+	{
+		c = toupper(s[i]);
+		if(c>='A' && c<='Z')
+		{
+			retcode = retcode << 5;
+			retcode = retcode | (c & 0x1F);
+		}
+		i++;
+	}
+
+	return retcode;
 }
